b78f89725a88098195e2eef9158603bc319e05e7
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-04-02 10:12:26 +0200

    Add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,29 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Counter from './components/Counter'
+import Header from './components/header'
+import './index.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Counter initialValue={80} />
+  },
+  {
+    path: '/load',
+    element: <div>test</div>
+  },
+
+])
+
+// console.log('Scipt: App')
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<>
+  {/* {console.log('Render: App')} */}
+  <div className="App">
+    <Header />
+    <main className="App-main">
+      <RouterProvider router={router} />
+    </main>
+  </div>
+</>);
 
