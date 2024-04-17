ef186881ba04182c1652ee9c1f774739b86ad422
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-04-17 16:05:50 +0200

    uncanny

--- src/views/todo.tsx
+++ src/views/todo.tsx
@@ -1,18 +1,19 @@
-import { KeyboardEvent, useState } from "react"
+import { KeyboardEvent, useEffect, useRef, useState } from "react"
 import Counter from "../components/counter"
 import Item from "../components/item"
 import './todo.css'
 
 export default function Todo() {
 
-    const [id, setId] = useState(0)
+    const id = useRef(0)
     const [items, setItems] = useState<{ id: number, text: string }[]>([])
     const [input, setInput] = useState('')
+    const i = useRef<HTMLInputElement>(null)
 
     function addItem({ key }: KeyboardEvent<HTMLInputElement>) {
         if (key === "Enter" && input) {
-            items.push({ id: id, text: input })
-            setId(id + 1)
+            items.push({ id: id.current, text: input })
+            id.current++
             setInput('')
         }
     }
@@ -21,11 +22,15 @@ export default function Todo() {
         setItems(items.filter(({ id }) => id !== rid))
     }
 
+    useEffect(() => {
+        i.current!.disabled = false
+    }, [])
+
     return <>
         <section id="todo">
             <label >
                 <h2>Add new item</h2>
-                <input id="input" value={input} onChange={(ev) => setInput(ev.target.value)} onKeyDown={addItem} />
+                <input ref={i} disabled id="input" value={input} onChange={(ev) => setInput(ev.target.value)} onKeyDown={addItem} />
             </label>
             <ul className="list">
                 {items.map(item => <Item key={item.id} item={item} remove={removeItem} />)}
