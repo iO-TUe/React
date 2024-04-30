cd98114db4a3bcaf5dba70e82ffd141d95049379
Joris <6518350+Joristdh@users.noreply.github.com>
2024-04-30 16:46:14 +0200

    Use global waitfor

--- src/views/todo.tsx
+++ src/views/todo.tsx
@@ -1,4 +1,4 @@
-import { KeyboardEvent, useEffect, useRef, useState } from "react"
+import { KeyboardEvent, useRef, useState } from "react"
 import Counter from "../components/counter.gen"
 import Item from "../components/item"
 import './todo.css'
@@ -6,12 +6,11 @@ import './todo.css'
 export default function Todo() {
     const id = useRef(0)
     const [items, setItems] = useState<{ id: number, text: string }[]>([])
-    const input = useRef<HTMLInputElement>(null)
 
-    function addItem({ key }: KeyboardEvent<HTMLInputElement>) {
-        if (key === "Enter" && input.current!.value) {
-            setItems([...items, { id: id.current++, text: input.current!.value }])
-            input.current!.value = ''
+    function addItem({ key, target }: KeyboardEvent<HTMLInputElement>) {
+        if (key === "Enter" && (target as HTMLInputElement).value) {
+            setItems([...items, { id: id.current++, text: (target as HTMLInputElement).value }]);
+            (target as HTMLInputElement).value = ''
         }
     }
 
@@ -19,17 +18,13 @@ export default function Todo() {
         setItems(items.filter(({ id }) => id !== rid))
     }
 
-    useEffect(() => {
-        input.current!.disabled = false
-    }, [])
-
     // console.log("Script: Todo")
     return <>
         {/* {console.log("Render: Todo")} */}
         <section id="todo">
             <label >
                 <h2>Add new item</h2>
-                <input ref={input} disabled id="input" onKeyUp={addItem} />
+                <input id="input" onKeyUp={addItem} />
             </label>
             <ul className="list">
                 {items.map(item => <Item key={item.id} item={item} remove={removeItem} />)}
